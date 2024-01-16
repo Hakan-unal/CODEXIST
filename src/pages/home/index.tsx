6da0571a5bbd0fcd7b11d60e3733daaef015cc48
Con Sınov <hakanunal_1996@hotmail.com>
2024-01-16 16:25:57 +0300

    deploy bug fixed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,34 +1,10 @@
-import { Row, Col, Card, Image, Statistic } from "antd";
-import { statistic } from "../../staticData/data";
-import yoda from "../../assets/yoda.webp";
+import { Card } from "antd";
 
 const Home = (props: any) => {
-   const StatisticComponent = () => {
-      return statistic.map((obj, index) => {
-         return (
-            <Col key={index} xs={24} sm={12} md={8}  xl={6}>
-               <Card bordered={false} hoverable>
-                  <Statistic
-                     title={obj.name}
-                     value={obj.length}
-                     valueStyle={{ color: "#3f8600" }}
-                  />
-               </Card>
-            </Col>
-         );
-      });
-   };
+   
    return (
       <Card style={{ textAlign: "center", fontSize: "4.3vh" }}>
-         <Row justify={"center"} gutter={[12, 36]}>
-            <Col>
-               <Card cover={<Image preview={false} alt="yoda" src={yoda} />}>
-                  Hey&#x1F44B;, It's Hakan. Currently working as Senior Frontend
-                  Developer. Here you can learn more about me and my ideas.
-                  <Row>{StatisticComponent()}</Row>
-               </Card>
-            </Col>
-         </Row>
+         
       </Card>
    );
 };
